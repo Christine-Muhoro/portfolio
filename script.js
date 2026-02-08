2bfbca86b0955ce0c547d760c5a86a1a56136f08
Christine-Muhoro <christinenmuhoro@gmail.com>
2026-02-09 00:07:30 +0300

    hiding and unhidng navbar

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const toggleBtn = document.getElementById("themeToggle");
+    /* ================= THEME TOGGLE ================= */
 
-    // Detect system preference
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.body.classList.add('dark');
-        toggleBtn.textContent = '☀️';
-    } else {
-        document.body.classList.add('light');
-        toggleBtn.textContent = '🌙';
+    const themeBtn = document.getElementById("themeToggle");
+
+    if (themeBtn) {
+
+        // Detect system preference
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.body.classList.add('dark');
+            themeBtn.textContent = '☀️';
+        } else {
+            document.body.classList.add('light');
+            themeBtn.textContent = '🌙';
+        }
+
+        themeBtn.addEventListener('click', () => {
+            if (document.body.classList.contains('dark')) {
+                document.body.classList.remove('dark');
+                document.body.classList.add('light');
+                themeBtn.textContent = '🌙';
+            } else {
+                document.body.classList.remove('light');
+                document.body.classList.add('dark');
+                themeBtn.textContent = '☀️';
+            }
+        });
     }
 
-    toggleBtn.addEventListener('click', () => {
-        if (document.body.classList.contains('dark')) {
-            document.body.classList.remove('dark');
-            document.body.classList.add('light');
-            toggleBtn.textContent = '🌙';
-        } else {
-            document.body.classList.remove('light');
-            document.body.classList.add('dark');
-            toggleBtn.textContent = '☀️';
-        }
-    });
+    /* ================= SIDEBAR TOGGLE ================= */
 
-    const btn = document.getElementById("sidebar-toggle");
+    const sidebarBtn = document.getElementById("sidebar-toggle");
     const sidebar = document.getElementById("sidebar");
 
-    btn.addEventListener("click", function () {
+    if (sidebarBtn && sidebar) {
+        sidebarBtn.addEventListener("click", function () {
 
-        // MOBILE
-        if (window.innerWidth <= 768){
-            sidebar.classList.toggle("show");
-            return;
-        }
+            // MOBILE
+            if (window.innerWidth <= 768){
+                sidebar.classList.toggle("show");
+                return;
+            }
 
-        // DESKTOP
-        sidebar.classList.toggle("closed");
-        document.body.classList.toggle("full");
-    });
+            // DESKTOP
+            sidebar.classList.toggle("closed");
+            document.body.classList.toggle("full");
 
+        });
+    }
 
 });
-
