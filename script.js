28582af9ce78f9bd8b183289acd10d1c219e2a35
Christine-Muhoro <christinenmuhoro@gmail.com>
2026-03-13 21:59:11 +0300

    more changes

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
         // Detect system preference
         if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
             document.body.classList.add('dark');
-            themeBtn.textContent = '☀️light mode';
+            themeBtn.textContent = '☀️';
         } else {
             document.body.classList.add('light');
-            themeBtn.textContent = '🌙dark mode';
+            themeBtn.textContent = '🌙';
         }
 
         themeBtn.addEventListener('click', () => {
             if (document.body.classList.contains('dark')) {
                 document.body.classList.remove('dark');
                 document.body.classList.add('light');
-                themeBtn.textContent = '🌙dark mode';
+                themeBtn.textContent = '🌙';
             } else {
                 document.body.classList.remove('light');
                 document.body.classList.add('dark');
-                themeBtn.textContent = '☀️light mode';
+                themeBtn.textContent = '☀️';
             }
         });
     }
